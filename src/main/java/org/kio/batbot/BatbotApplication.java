070ea326dc2feb6346a2d4b13e8f2be2bf3905e1
Kiio__ <caillaud.hugo.44@gmail.com>
2020-02-17 20:22:45 +0100

    Initial Commit

---
+++ src/main/java/org/kio/batbot/BatbotApplication.java
@@ -0,0 +1,27 @@
+package org.kio.batbot;
+
+import org.kio.batbot.bo.Command;
+import org.kio.batbot.bo.CommandCategory;
+import org.kio.batbot.bo.CommandDetail;
+import org.kio.batbot.bo.User;
+import org.kio.batbot.dal.ICommandRepository;
+import org.kio.batbot.dal.IUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.rest.core.config.RepositoryRestConfiguration;
+import org.springframework.data.rest.webmvc.config.RepositoryRestConfigurerAdapter;
+
+@SpringBootApplication
+public class BatbotApplication extends RepositoryRestConfigurerAdapter {
+
+    @Autowired
+    private IUserRepository UserDAL;
+    private ICommandRepository CommandDAL;
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatbotApplication.class, args);
+        System.out.println("BatBot API just Started !");
+    }
+
+}
---
+++ src/test/java/org/kio/batbot/BatbotApplicationTests.java
@@ -0,0 +1,13 @@
+package org.kio.batbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatbotApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
